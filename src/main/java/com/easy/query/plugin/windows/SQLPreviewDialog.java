0b06cf2a779d67567cec182142e35d400e7cf354
xuejiaming <326308290@qq.com>
2023-11-16 23:58:49 +0800

    支持sql无参

--- src/main/java/com/easy/query/plugin/windows/SQLPreviewDialog.java
+++ src/main/java/com/easy/query/plugin/windows/SQLPreviewDialog.java
@@ -2,6 +2,7 @@ package com.easy.query.plugin.windows;
 
 import com.easy.query.plugin.core.util.BasicFormatter;
 import com.easy.query.plugin.core.util.DialogUtil;
+import com.intellij.openapi.project.Project;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
@@ -10,9 +11,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -22,7 +31,24 @@ public class SQLPreviewDialog extends JDialog {
     private JTextArea selectSQLText;
     private JTextArea previewSQLText;
     private JButton convertButton;
+    private static final char MARK = '?';
     private static final BasicFormatter FORMATTER = new BasicFormatter();
+
+    private static final Set<String> NEED_BRACKETS;
+
+
+    static {
+        Set<String> types = new HashSet<>(8);
+        types.add("String");
+        types.add("Date");
+        types.add("Time");
+        types.add("LocalDate");
+        types.add("LocalTime");
+        types.add("LocalDateTime");
+        types.add("BigDecimal");
+        types.add("Timestamp");
+        NEED_BRACKETS = Collections.unmodifiableSet(types);
+    }
 
     public SQLPreviewDialog(String selectSQL) {
         setContentPane(contentPane);
@@ -62,6 +88,7 @@ public class SQLPreviewDialog extends JDialog {
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
+
     private void onCancel() {
         // add your code here if necessary
         dispose();
@@ -74,92 +101,99 @@ public class SQLPreviewDialog extends JDialog {
 //        System.exit(0);
 //    }
 
-    private void onConvert(){
+    private void onConvert() {
+        try {
+            onConvert0();
+        } catch (Exception ex) {
+            previewSQLText.setText("转换出现异常:" + ex.getMessage());
+        }
+    }
+
+    private void onConvert0() {
         String selectSQL = selectSQLText.getText();
-        if(StringUtils.isBlank(selectSQL)){
+        if (StringUtils.isBlank(selectSQL)) {
             previewSQLText.setText("");
-        }else{
+        } else {
             StringBuilder sqlBuilder = new StringBuilder();
             String[] selectSQLs = selectSQL.split("\n");
             List<String> sqlList = Arrays.stream(selectSQLs)
                     .filter(o -> StringUtils.contains(o, "Preparing:"))
-                    .map(o -> o.replaceAll(".*(Preparing[\\s]*(?=:)):", ""))
+                    .map(o -> o.replaceAll(".*(Preparing[\\s]*(?=:)): ", ""))
                     .collect(Collectors.toList());
-            List<List<String>> parameterList = Arrays.stream(selectSQLs)
+            List<Queue<Map.Entry<String, String>>> parameterList = Arrays.stream(selectSQLs)
                     .filter(o -> StringUtils.contains(o, "Parameters:"))
-                    .map(o -> parseParas(o))
+                    .map(o -> parseParams(o))
                     .collect(Collectors.toList());
             int min = Math.min(sqlList.size(), parameterList.size());
             for (int i = 0; i < min; i++) {
                 String sql = sqlList.get(i);
-                List<String> params = parameterList.get(i);
-                for (int i1 = 0; i1 < params.size(); i1++) {
-                    sql=sql.replace("?",params.get(i1));
-                }
+                Queue<Map.Entry<String, String>> params = parameterList.get(i);
+
+                sql = parseSql(sql, params).toString();
 
                 String formatSQL = FORMATTER.format(sql);
-                if(i!=0){
-                    sqlBuilder.append("\n-- 第").append(i).append("条sql数据\n");
-                }
+                sqlBuilder.append("\n-- 第").append(i + 1).append("条sql数据\n");
                 sqlBuilder.append(formatSQL);
-            }
-            if(min>1){
-                sqlBuilder.insert(0,"-- 第1条sql数据\n");
             }
 
             previewSQLText.setText(sqlBuilder.toString());
-            convertButton.setText("转换↓ 一共转换成"+min+"条sql");
+            convertButton.setText("转换↓ 一共转换成" + min + "条sql");
         }
     }
-    private static String parsePara(String p) {
-        if ("null".equals(p)) {
-            return p;
+
+    private StringBuilder parseSql(String sql, Queue<Map.Entry<String, String>> params) {
+
+        final StringBuilder sb = new StringBuilder(sql);
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) != MARK) {
+                continue;
+            }
+
+            final Map.Entry<String, String> entry = params.poll();
+            if (Objects.isNull(entry)) {
+                continue;
+            }
+
+
+            sb.deleteCharAt(i);
+
+            if (NEED_BRACKETS.contains(entry.getValue())) {
+                if("LocalDateTime".equals(entry.getValue())){
+                    sb.insert(i, String.format("'%s'", entry.getKey().replace("T"," ")));
+                }else{
+                    sb.insert(i, String.format("'%s'", entry.getKey()));
+                }
+            } else {
+                sb.insert(i, entry.getKey());
+            }
+
+
         }
 
-        String result;
-        String m = p.replaceAll(".*\\(([^\\)]+)\\)$", "$1");
-
-        result = p.replaceAll("\\(" + m + "\\)", "");
-
-        switch (m) {
-            case "String":
-            case "Timestamp":
-            case "BigDecimal":
-            case "Date":
-            case "Time":
-            case "LocalDate":
-            case "LocalTime":
-                result = "'" + result + "'";
-                break;
-            case "LocalDateTime":
-                result = "'" + result.replace("T", " ") + "'";
-                break;
-//            case "Integer":
-//                // Handle Integer case if needed
-//                break;
-//            case "Date":
-//                result = "STR_TO_DATE('" + result + "','%Y-%m-%d %H:%i:%s')";
-//                break;
-            default:
-                // Handle other cases if needed
-                break;
-        }
-
-        return result;
+        return sb;
     }
-    private  List<String> parseParas(String para) {
-        List<String> ps = new ArrayList<>();
-        String p;
-        String result = para.replaceAll(".*(Parameters[\\s]*(?=:)):", "");
-        String[] t = result.split("(?=[\\s]*,)|(?<=[\\s]*\\$)");
 
-        int len = t.length;
-        if (len > 0) {
-            for (String s : t) {
-                p = s.replaceAll(",$", "").replaceAll("^[,\\s]+|[\\s]+$", "");
-                ps.add(parsePara(p));
+    private Queue<Map.Entry<String, String>> parseParams(String line) {
+        if(StringUtils.isBlank(line)){
+            return new ArrayDeque<>(0);
+        }
+        line = StringUtils.removeEnd(line, "\n").replaceAll(".*(Parameters[\\s]*(?=:)): ", "");
+
+        final String[] strings = StringUtils.splitByWholeSeparator(line, ",");
+        final Queue<Map.Entry<String, String>> queue = new ArrayDeque<>(strings.length);
+
+        for (String s : strings) {
+            String trim = StringUtils.trim(s);
+            String value = StringUtils.substringBeforeLast(trim, "(");
+            String type = StringUtils.substringBetween(trim, "(", ")");
+            if (StringUtils.isEmpty(type)) {
+                queue.offer(new AbstractMap.SimpleEntry<>(value, null));
+            } else {
+                queue.offer(new AbstractMap.SimpleEntry<>(value, type));
             }
         }
-        return ps;
+
+        return queue;
     }
 }
