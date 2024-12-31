281bde33953539042300566cf6e734a9ff0b2880
link2fun <19241982+link2fun@users.noreply.github.com>
2024-12-31 11:41:07 +0800

    feat: NavigateFlat 支持导航到PATH

--- src/main/java/com/easy/query/plugin/core/provider/NavToEntityFieldRelatedItemLineMarkerProvider.java
+++ src/main/java/com/easy/query/plugin/core/provider/NavToEntityFieldRelatedItemLineMarkerProvider.java
@@ -5,12 +5,16 @@ import com.easy.query.plugin.core.util.PsiJavaClassUtil;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.lang.jvm.annotation.JvmAnnotationAttribute;
+import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.impl.source.tree.java.PsiNameValuePairImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * DTO字段跳转到实体字段
@@ -50,6 +54,28 @@ public class NavToEntityFieldRelatedItemLineMarkerProvider extends RelatedItemLi
                         .setTooltipText("Navigate to Entity Field")
                         .createLineMarkerInfo(field.getIdentifyingElement());
                 result.add(navInfo);
+            }
+
+        }
+
+        // 看看字段上有没有 NavigateFlat 注解 com.easy.query.core.annotation.NavigateFlat
+        PsiAnnotation annoNavigateFlat = field.getAnnotation("com.easy.query.core.annotation.NavigateFlat");
+        if (Objects.nonNull(annoNavigateFlat)) {
+            // 有这个注解, 看看 pathAlias 是否有值
+            JvmAnnotationAttribute pathAlias = annoNavigateFlat.findAttribute("pathAlias");
+            if (pathAlias != null && pathAlias instanceof PsiNameValuePairImpl && ((PsiNameValuePairImpl) pathAlias).getLiteralValue()!=null) {
+                String pathAliasStr = ((PsiNameValuePairImpl) pathAlias).getLiteralValue();
+                // 有这个字段, 从当前类中找到静态字段
+                PsiField defineMappingPath = containingClass.findFieldByName(pathAliasStr, true);
+
+                // 添加链接
+                if (defineMappingPath != null) {
+                    RelatedItemLineMarkerInfo<PsiElement> navInfo = NavigationGutterIconBuilder.create(Icons.EQ)
+                            .setTargets(defineMappingPath)
+                            .setTooltipText("Navigate to Mapping Define")
+                            .createLineMarkerInfo(field.getIdentifyingElement());
+                    result.add(navInfo);
+                }
             }
         }
 
