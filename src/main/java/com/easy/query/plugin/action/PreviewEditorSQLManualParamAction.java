9a1c7965996584ab06ec39a93c49c5e30bf17ccc
link2fun <19241982+link2fun@users.noreply.github.com>
2025-02-28 09:36:03 +0800

    feat: 解决线程过期警告

--- src/main/java/com/easy/query/plugin/action/PreviewEditorSQLManualParamAction.java
+++ src/main/java/com/easy/query/plugin/action/PreviewEditorSQLManualParamAction.java
@@ -1,6 +1,8 @@
 package com.easy.query.plugin.action;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * 预览SQL 手动参数 并生成运行配置, 运行后不删除代码
@@ -10,8 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PreviewEditorSQLManualParamAction extends PreviewEditorSQLAbstractAction {
 
-
     public PreviewEditorSQLManualParamAction() {
         super("manual");
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        // 使用EDT线程更新UI操作
+        return ActionUpdateThread.EDT;
     }
 }
