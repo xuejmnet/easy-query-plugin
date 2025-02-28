48dfd1b23c41caaee32e54651ef1b50408fa164a
link2fun <19241982+link2fun@users.noreply.github.com>
2025-02-28 15:08:03 +0800

    feat: CompileAll 增加完成通知

--- src/main/java/com/easy/query/plugin/action/CompileAllAction.java
+++ src/main/java/com/easy/query/plugin/action/CompileAllAction.java
@@ -1,6 +1,7 @@
 package com.easy.query.plugin.action;
 
 import com.easy.query.plugin.core.startup.ProjectStartupHelper;
+import com.easy.query.plugin.core.util.NotificationUtils;
 import com.easy.query.plugin.core.util.PsiJavaFileUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -10,16 +11,16 @@ public class CompileAllAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
         Project project = e.getProject();
-//        VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-        PsiJavaFileUtil.createAptFile(project);
-
         if (project == null) {
             return;
         }
+        PsiJavaFileUtil.createAptFile(project);
+
         // 更新 generated sources root ，生成的代码需要标记一下
         ProjectStartupHelper.updateGeneratedSourceRoot(project);
 
+        // 添加气泡提醒
+        NotificationUtils.notifySuccess("编译完成", "EasyQuery", project);
     }
 }
