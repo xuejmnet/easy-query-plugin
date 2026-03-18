1b6259ebee202f2d794a2d758cdf62b5cd8c4ad7
xuejiaming <326308290@qq.com>
2026-03-18 08:34:44 +0800

    build: 0.1.88 support 2026.1+

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.easy-query"
-version = "0.1.87"
+version = "0.1.88"
 repositories {
     maven {
         setUrl("https://maven.aliyun.com/repository/central/")
@@ -64,7 +64,7 @@ tasks {
         //插件起始支持版本
         sinceBuild.set("231")
         //插件结束支持版本
-        untilBuild.set("253.*")
+        untilBuild.set("261.*")
     }
 
     signPlugin {
